182472a95320fa160f7f04665bf96b2232d37ed1
cloudcorp30-bera <cloudcorp30@gmail.com>
2026-01-29 12:56:49 +0300

    Update and rename index.js to index.cjs

---
+++ data/index.cjs
@@ -0,0 +1,9 @@
+const Handler = require('./handler.cjs');
+const Callupdate = require('./call-handler.cjs');
+const GroupUpdate = require('./group-handler.cjs');
+
+module.exports = {
+    Handler,
+    Callupdate,
+    GroupUpdate
+};
--- data/index.js
+++
@@ -1,9 +0,0 @@
-const Handler = require('./handler.js');
-const Callupdate = require('./call-handler.js');
-const GroupUpdate = require('./group-handler.js');
-
-module.exports = {
-    Handler,
-    Callupdate,
-    GroupUpdate
-};
