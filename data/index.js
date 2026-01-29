fd3068da19d685c98b9b51031160ba8e51bf2eec
cloudcorp30-bera <cloudcorp30@gmail.com>
2026-01-29 12:01:49 +0300

    Create index.js

---
+++ data/index.js
@@ -0,0 +1,9 @@
+const Handler = require('./handler.js');
+const Callupdate = require('./call-handler.js');
+const GroupUpdate = require('./group-handler.js');
+
+module.exports = {
+    Handler,
+    Callupdate,
+    GroupUpdate
+};
