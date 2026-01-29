02a89e36c803b77f9f4e2238941421b8e295422b
cloudcorp30-bera <cloudcorp30@gmail.com>
2026-01-29 12:02:25 +0300

    Create call-handler.js

---
+++ data/call-handler.js
@@ -0,0 +1,15 @@
+const config = require('../config.cjs');
+
+const Callupdate = async (json, sock) => {
+   for (const id of json) {
+      if (id.status === 'offer' && config.REJECT_CALL ) {
+         let msg = await sock.sendMessage(id.from, {
+            text: `*_📞 Auto Reject Call Mode Activated_* \n*_📵 No Calls Allowed_*`,
+            mentions: [id.from],
+         });
+         await sock.rejectCall(id.id, id.from);
+      }
+   }
+};
+
+module.exports = Callupdate;
